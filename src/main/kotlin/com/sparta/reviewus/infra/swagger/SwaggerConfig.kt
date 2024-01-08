0dbe349a4e1f8619207354eeb20fbdac9fa19783
moveuk <v_donguk@naver.com>
2024-01-08 16:46:36 +0900

    ✨Feat: 스웨거 테스트 구성 세팅

---
+++ src/main/kotlin/com/sparta/reviewus/infra/swagger/SwaggerConfig.kt
@@ -0,0 +1,21 @@
+package com.sparta.reviewus.infra.swagger
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun openAPI(): OpenAPI = OpenAPI()
+        .components(Components())
+        .info(
+            Info()
+                .title("ReviewUs API")
+                .description("ReviewUs API schema")
+                .version("0.0.0")
+        )
+}
