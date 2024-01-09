d2d31dee74b75b203f3a71c25cfa8e7e3e7973bc
moveuk <v_donguk@naver.com>
2024-01-09 18:24:37 +0900

    ✨Feat: authenticate 기능 추가
    1. authenticate Member 기능 추가
    2. swagger basic auth 기능 설청 추가
    3. 이메일 비밀번호 틀릴시 BadCredentialsException 예외처리
    4. GlobalHandler에 BadCredentialsException 추가
    
    Resolves: #1

--- src/main/kotlin/com/sparta/reviewus/infra/swagger/SwaggerConfig.kt
+++ src/main/kotlin/com/sparta/reviewus/infra/swagger/SwaggerConfig.kt
@@ -3,6 +3,8 @@ package com.sparta.reviewus.infra.swagger
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -11,11 +13,18 @@ class SwaggerConfig {
 
     @Bean
     fun openAPI(): OpenAPI = OpenAPI()
-        .components(Components())
+        .components(
+            Components().addSecuritySchemes("basicAuth", basicAuth())
+        )
+        .addSecurityItem(securityItem())
         .info(
             Info()
                 .title("ReviewUs API")
                 .description("ReviewUs API schema")
                 .version("0.0.0")
         )
+
+    private fun basicAuth() = SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")
+
+    private fun securityItem() = SecurityRequirement().addList("basicAuth")
 }
