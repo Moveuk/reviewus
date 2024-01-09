1f8a20e62e4432917521e4e5e194011d94453dd7
hjm <gks777777@gmail.com>
2024-01-09 16:44:58 +0900

    📝Fix: 테스트 후 오류 수정
    address와 interest의 ?를 추가했습니다.

--- src/main/kotlin/com/sparta/reviewus/member/dto/MemberResponse.kt
+++ src/main/kotlin/com/sparta/reviewus/member/dto/MemberResponse.kt
@@ -7,6 +7,6 @@ data class MemberResponse(
     val profilePicUrl: String,
     val nickname: String,
     val introduction: String,
-    val address: String,
-    val interest: String,
+    val address: String?,
+    val interest: String?,
 )
