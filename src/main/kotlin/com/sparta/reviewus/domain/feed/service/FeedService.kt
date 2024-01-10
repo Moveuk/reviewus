b204cc408d8dd03adecb6014670c4e690036181b
jeawonlee0327 <dlwpdnjs0327@gmail.com>
2024-01-10 16:08:13 +0900

    feat: controller, service 수정
    1 단건조회, 전체조회, 삭제기능 구현

--- src/main/kotlin/com/sparta/reviewus/domain/feed/service/FeedService.kt
+++ src/main/kotlin/com/sparta/reviewus/domain/feed/service/FeedService.kt
@@ -6,13 +6,11 @@ import com.sparta.reviewus.domain.feed.dto.CreateFeedResponse
 interface FeedService {
     fun createFeed(request: CreateFeedRequest): CreateFeedResponse
 
-//    fun getFeed(request: CreateFeedRequest): FeedResponse
-//
-//    fun getFeedList(): List<FeedResponse>
-//
-//    fun getFeedById(Id: Long): FeedResponse
-//
-//    fun updateFeed(title: String, request: CreateFeedRequest): FeedResponse
-//
-//    fun deleteFeed(title: String)
+    fun getFeedList(): List<CreateFeedResponse>
+
+    fun getFeedById(id: Long): CreateFeedResponse
+
+//    fun updateFeed(title: String, request: CreateFeedRequest): CreateFeedResponse
+
+    fun deleteFeed(id: Long)
 }
--- src/main/kotlin/com/sparta/reviewus/domain/feed/service/FeedServiceImpl.kt
+++ src/main/kotlin/com/sparta/reviewus/domain/feed/service/FeedServiceImpl.kt
@@ -9,6 +9,7 @@ import com.sparta.reviewus.domain.feed.repository.FeedRepository
 import com.sparta.reviewus.domain.member.repository.MemberRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 
 @Service
@@ -16,6 +17,7 @@ class FeedServiceImpl(
     private val feedRepository: FeedRepository,
     private val memberRepository: MemberRepository
 ) : FeedService {
+    @Transactional
     override fun createFeed(request: CreateFeedRequest): CreateFeedResponse {
         val member = memberRepository.findByIdOrNull(request.memberId) ?: throw ModelNotFoundException("Member",request.memberId) //이거는 무슨일을 하는 코드
         return feedRepository.save(
@@ -31,4 +33,17 @@ class FeedServiceImpl(
         ).toResponse()
     }
 
+    override fun getFeedList(): List<CreateFeedResponse> {
+        return feedRepository.findAll().map { it. toResponse()}
+    }
+
+    override fun getFeedById(id: Long): CreateFeedResponse {
+        val feed = feedRepository.findByIdOrNull(id) ?: throw ModelNotFoundException("feed", id)
+        return feed.toResponse()
+    }
+    @Transactional
+    override fun deleteFeed(id: Long) {
+        val feed = feedRepository.findByIdOrNull(id) ?: throw ModelNotFoundException("feed", id)
+        feedRepository.delete(feed)
+    }
 }
