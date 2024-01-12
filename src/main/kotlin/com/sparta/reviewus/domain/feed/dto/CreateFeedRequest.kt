2530fddc417f8bac9d6d725fdf31167fb9a7e207
jeawonlee0327 <dlwpdnjs0327@gmail.com>
2024-01-12 22:33:41 +0900

    ✨Feat: feed create, update, delete 인증인가 처리 구현
    1. feed create ,update , delete 인자 추가
    2. FeedService 랑 impl 메소드 순서 정렬
    3. feed ServiceImpl 에서 예외처리 기능 추가
    4. feed service 인자 추가
    5. feed controller 인자 추가 및 메소드 순서 정렬

--- src/main/kotlin/com/sparta/reviewus/domain/feed/dto/CreateFeedRequest.kt
+++ src/main/kotlin/com/sparta/reviewus/domain/feed/dto/CreateFeedRequest.kt
@@ -3,10 +3,9 @@ package com.sparta.reviewus.domain.feed.dto
 import com.sparta.reviewus.domain.feed.model.Category
 
 data class CreateFeedRequest(
-    val memberId : Long,
     val category : Category,
     val title : String,
-    val feedPicUrl : String,
+    val feedPicUrls : String,
     val longitude : String?,
     val latitude : String?,
     val description : String?
--- src/main/kotlin/com/sparta/reviewus/domain/feed/service/FeedService.kt
+++ src/main/kotlin/com/sparta/reviewus/domain/feed/service/FeedService.kt
@@ -4,15 +4,16 @@ import com.sparta.reviewus.domain.feed.dto.CreateFeedRequest
 import com.sparta.reviewus.domain.feed.dto.FeedByIdResponse
 import com.sparta.reviewus.domain.feed.dto.FeedResponse
 import com.sparta.reviewus.domain.feed.dto.UpdateFeedRequest
+import com.sparta.reviewus.domain.member.dto.AuthenticatedMember
 
 interface FeedService {
-    fun createFeed(request: CreateFeedRequest): FeedResponse
-
     fun getFeeds(): List<FeedResponse>
 
-    fun getFeedById(id: Long, memberId:Long?): FeedByIdResponse
+    fun getFeedById(feedId: Long, authenticatedMember: AuthenticatedMember?): FeedByIdResponse
 
-    fun updateFeed(id: Long, request: UpdateFeedRequest): FeedResponse
+    fun createFeed(request: CreateFeedRequest, authenticatedMember: AuthenticatedMember): FeedResponse
 
-    fun deleteFeed(id: Long)
+    fun updateFeed(feedId: Long, request: UpdateFeedRequest, authenticatedMember: AuthenticatedMember): FeedResponse
+
+    fun deleteFeed(feedId: Long, authenticatedMember: AuthenticatedMember)
 }
