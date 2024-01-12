6cc6d33552760e034f94dc7d202a02c4537f41a3
lazzzykim <33sunghyeon@gmail.com>
2024-01-12 20:17:16 +0900

    🔨 Exception 위치수정

--- src/main/kotlin/com/sparta/reviewus/domain/exception/WrongPasswordException.kt
+++ src/main/kotlin/com/sparta/reviewus/domain/exception/WrongPasswordException.kt
@@ -1,4 +1,4 @@
-package com.sparta.reviewus.domain.exception.reply
+package com.sparta.reviewus.domain.exception
 
 data class WrongPasswordException(override val message: String = "패스워드가 일치하지 않습니다." )
     : RuntimeException(message)
